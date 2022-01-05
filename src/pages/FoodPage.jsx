ab490ffab8760aedb56f516324de350aa93edec2
GabrielModa <gabriel-moda@hotmail.com>
2022-01-05 14:42:22 -0300

    fixing bug

--- src/pages/FoodPage.jsx
+++ src/pages/FoodPage.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useContext } from 'react';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 import { RecipesContext } from '../contexts/RecipesContext';
 import { foodRecipesAPI } from '../fetchApi/fetchApi';
 
 export default function FoodPage() {
   const { state, setStateGlobal, foodRecipes } = useContext(RecipesContext);
 
+  const AMOUNT_NUMBER = 12;
+
   const requestAPI = async () => {
     const food = await foodRecipesAPI();
+    console.log(food);
     const foodFiltered = food.filter((food, index) => {
-      if (index <= 12) {
+      if (index <= AMOUNT_NUMBER) {
         return food;
       }
     });
@@ -24,10 +28,27 @@ export default function FoodPage() {
   return (
 
     <div>
+      <Header />
       {foodRecipes && foodRecipes.map((card, index) => (
+        (<div
+          key={ card.idMeal }
+        >
+          <img
+            src={ card.strMealThumb }
+            alt=""
+            data-testid={ ` ${index}-card-img ` }
+          />
+          <h3 data-testid={ ` ${index}-card-name ` }>
+            {card.strMeal}
 
-        (<div key={ card.idMeal }>{card.idMeal}</div>)
+          </h3>
+          <p
+            data-testid={ ` ${index}-recipe-card ` }
+          >
+            {card.strSource}
 
+          </p>
+        </div>)
       ))}
       Food Page
       <Footer />
