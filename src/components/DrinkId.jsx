d2efc9e5c26057e8cd6ac53a7cd23c7e3571882d
Carlos Afonso <carlos.flach15@gmail.com>
2022-01-06 18:40:28 -0300

    Drink details page.

--- src/components/DrinkId.jsx
+++ src/components/DrinkId.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { RecipesContext } from '../contexts/RecipesContext';
+import DrinksRecommended from './DrinksRecommended';
 
 export default function DrinkId() {
   const { saveDrinkRecipes } = useContext(RecipesContext);
@@ -19,10 +20,32 @@ export default function DrinkId() {
       <h3 data-testid="recipe-title">{element.strDrink}</h3>
       <h4 data-testid="recipe-category">{`Categoria: ${element.strCategory}`}</h4>
       <img src={ element.strDrinkThumb } alt={ `${element.strDrink}` } />
-      {ingredients}
-      <div className='recipe-details-btns'>
-        <button type='submit' data-testid="share-btn">Compartilhar Receita</button>
-        <button type='submit' data-testid="favorite-btn">Favoritar</button>
+      <ul>
+        {ingredients.map((ing, index) => (
+          <li
+            key={ ing }
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            {ing}
+          </li>
+        ))}
+      </ul>
+      <p data-testid="instructions">
+        {element.strInstructions}
+      </p>
+      <video width="250" height="250" controls data-testid="video">
+        <track kind="captions" />
+        <source src={ element.strVideo } type="video/mp4" />
+        <source src="movie.ogg" type="video/ogg" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="recipe-details-btns">
+        <button type="button" data-testid="share-btn">Compartilhar Receita</button>
+        <button type="button" data-testid="favorite-btn">Favoritar</button>
+        <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+      </div>
+      <div className="recommended-recipes">
+        <DrinksRecommended />
       </div>
     </div>
   );
