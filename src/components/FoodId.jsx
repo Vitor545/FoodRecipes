1147b0865edc0fc26ff65b72bda8f0cc5b01be79
Joao-Ortega <joao_ortega17@hotmail.com>
2022-01-07 15:39:09 -0300

    in progress

--- src/components/FoodId.jsx
+++ src/components/FoodId.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { foodDetailsRequest } from '../fetchApi/fetchApi';
 import FoodsRecommended from './FoodsRecommended';
+import FavoriteBtn from './FavoriteBtn';
+import ShareBtn from './ShareBtn';
+import StartRecipeBtn from './StartRecipeBtn';
 import { RecipesContext } from '../contexts/RecipesContext';
 
 export default function FoodId() {
@@ -19,8 +22,12 @@ export default function FoodId() {
     // Pegando todas as chaves de foodDetails que contenham Ingredient no nome
     const ingr = Object.keys(foodDetails[0])
       .filter((key) => key.includes('Ingredient'));
+    const measure = Object.keys(foodDetails[0])
+      .filter((key) => key.includes('Measure'));
     // Fazendo um map pelas chaves e pegando os valores dessas chaves em foodDetails
     const values = ingr.map((ingredient) => foodDetails[0][ingredient])
+      .filter((el) => el !== '');
+    const valuesMeasure = ingr.map((qty) => foodDetails[0][qty])
       .filter((el) => el !== '');
     return (
       values.map((ing, i) => (
@@ -28,7 +35,7 @@ export default function FoodId() {
           key={ ing }
           data-testid={ `${i}-ingredient-name-and-measure` }
         >
-          {ing}
+          {`${ing} - ${valuesMeasure[i]}`}
         </li>
       ))
     );
@@ -40,27 +47,36 @@ export default function FoodId() {
 
   return (
     <div>
-      { foodDetails && foodDetails
-        .map(({ idMeal, strMeal, strCategory, strMealThumb, strInstructions }) => (
-          <div key={ idMeal }>
-            <h3 data-testid="recipe-title">{strMeal}</h3>
-            <h4 data-testid="recipe-category">{`Categoria: ${strCategory}`}</h4>
-            <img
-              src={ strMealThumb }
-              alt={ `${strMeal}` }
-              data-testid="recipe-photo"
-            />
-            <ul>
-              {renderIngredients()}
-            </ul>
-            <p data-testid="instructions">
-              {strInstructions}
-            </p>
-          </div>
-        ))}
+      { foodDetails
+      && foodDetails.map((
+        { idMeal, strMeal, strCategory, strMealThumb, strInstructions, strYoutube },
+      ) => (
+        <div key={ idMeal }>
+          <h3 data-testid="recipe-title">{strMeal}</h3>
+          <h4 data-testid="recipe-category">{`Categoria: ${strCategory}`}</h4>
+          <img
+            src={ strMealThumb }
+            alt={ `${strMeal}` }
+            data-testid="recipe-photo"
+          />
+          <ul>
+            {renderIngredients()}
+          </ul>
+          <p data-testid="instructions">
+            {strInstructions}
+          </p>
+          <video width="320" height="240" controls data-testid="video">
+            <track kind="captions" />
+            <source src={ strYoutube } type="video/mp4" />
+            <source src="movie.ogg" type="video/ogg" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      ))}
       <div>
-        <button type="button" data-testid="favorite-btn">Favoritar</button>
-        <button type="button" data-testid="start-recipe-btn">Iniciar Receita</button>
+        <ShareBtn />
+        <FavoriteBtn />
+        <StartRecipeBtn />
       </div>
       <div className="recommended-recipes ">
         <FoodsRecommended />
