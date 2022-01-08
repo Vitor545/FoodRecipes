45059151b03bee055317f00c21f6ceb2c874b909
Joao-Ortega <joao_ortega17@hotmail.com>
2022-01-08 13:30:59 -0300

    Trying to fix problem

--- src/components/StartRecipeBtn.jsx
+++ src/components/StartRecipeBtn.jsx
@@ -1,49 +1,69 @@
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { RecipesContext } from '../contexts/RecipesContext';
+import { drinkDetailsRequest } from '../fetchApi/fetchApi';
 
 export default function StartRecipeBtn() {
-  const { state, isStarted, foodDetails, setStateGlobal } = useContext(RecipesContext);
+  const { state, isStarted, foodDetails,
+    drinkDetails, setStateGlobal } = useContext(RecipesContext);
   const { id } = useParams();
-  let mealToLocal = JSON.parse(localStorage.getItem('inProgressRecipes'));
-  if (!mealToLocal) {
-    mealToLocal = {
+  // const inProgressRecipes = {
+  //   cocktails: {},
+  //   meals: {},
+  // };
+  let infoFromLocal = JSON.parse(localStorage.getItem('inProgressRecipes'));
+  if (!infoFromLocal) {
+    infoFromLocal = {
       cocktails: {},
       meals: {},
     };
   }
-  const ingr = Object.keys(foodDetails[0])
-    .filter((key) => key.includes('Ingredient'));
-  const values = ingr.map((ingredient) => foodDetails[0][ingredient])
-    .filter((el) => el !== '');
-  // const measure = Object.keys(foodDetails[0])
-  //   .filter((key) => key.includes('Measure'));
-  // const valuesMeasure = measure.map((qty) => foodDetails[0][qty])
-  //   .filter((el) => el !== '');
-  // const ingrList = values.map((ing, i) => (
-  //   `${ing} - ${valuesMeasure[i]}`
-  // ));
-  // setStateGlobal({ ...state, foodIngrList: ingrList });
 
-  const handleClick = () => {
-    const inProgressRecipes = {
-      cocktails: {},
-      meals: {},
-    };
-    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipes));
-    mealToLocal.meals = { ...mealToLocal.meals, [id]: values };
-    localStorage.setItem('inProgressRecipes', JSON.stringify(mealToLocal));
+  // const ingr = Object.keys(foodDetails[0])
+  //   .filter((key) => key.includes('Ingredient'));
+  // const values = ingr.map((ingredient) => foodDetails[0][ingredient])
+  //   .filter((el) => el !== '');
+  // const drinkIngr = Object.keys(drinkDetails[0])
+  //   .filter((key) => key.includes('Ingredient'));
+  // const drinkValues = ingr.map((ingredient) => drinkDetails[0][ingredient])
+  //   .filter((el) => el !== null);
+  // const valuesMeasure = drinkValues.map((qty) => drinkDetails[0][qty])
+  //   .map((el) => (el === null ? '' : el));
+
+  const teste = async () => {
+    const some = await drinkDetailsRequest(id);
+    return some;
+  };
+
+  const handleDrinkClick = () => {
+    const drinkIngr = Object.keys(drinkDetails[0])
+      .filter((key) => key.includes('Ingredient'));
+    const drinkValues = drinkIngr.map((ingredient) => drinkDetails[0][ingredient])
+      .filter((el) => el !== null);
+    infoFromLocal.cocktails = { ...infoFromLocal.cocktails, [id]: drinkValues };
+    localStorage.setItem('inProgressRecipes', JSON.stringify(infoFromLocal));
     if (!isStarted) setStateGlobal({ ...state, isStarted: true });
-    console.log(mealToLocal);
+    // console.log(infoFromLocal);
+  };
+
+  const handleFoodClick = () => {
+    const ingr = Object.keys(foodDetails[0])
+      .filter((key) => key.includes('Ingredient'));
+    const values = ingr.map((ingredient) => foodDetails[0][ingredient])
+      .filter((el) => el !== '');
+    infoFromLocal.meals = { ...infoFromLocal.meals, [id]: values };
+    localStorage.setItem('inProgressRecipes', JSON.stringify(infoFromLocal));
+    if (!isStarted) setStateGlobal({ ...state, isStarted: true });
   };
 
   useEffect(() => {
-    const isSaved = Object.keys(mealToLocal.meals).includes(id);
+    const isSaved = Object.keys(infoFromLocal.meals).includes(id)
+      || Object.keys(infoFromLocal.cocktails).includes(id);
     console.log(isSaved);
     if (isSaved) {
-      setStateGlobal({ ...state, isStarted: true });
-    } else {
       setStateGlobal({ ...state, isStarted: false });
+    } else {
+      setStateGlobal({ ...state, isStarted: true });
     }
   }, []);
 
@@ -52,9 +72,9 @@ export default function StartRecipeBtn() {
       type="button"
       data-testid="start-recipe-btn"
       className="start-btn"
-      onClick={ handleClick }
+      onClick={ teste() === null ? handleFoodClick() : handleDrinkClick() }
     >
-      { isStarted ? 'Continue Recipe' : 'Start Recipe' }
+      { isStarted ? 'Continuar Receita' : 'Iniciar Receita' }
     </button>
   );
 }
