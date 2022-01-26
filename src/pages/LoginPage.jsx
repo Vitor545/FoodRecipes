240a593ea9fd332263597a30fedeb7e7b61c28fb
vitor <tutornews2@gmail.com>
2022-01-26 14:57:12 -0300

    Pagina Login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router';
 import { RecipesContext } from '../contexts/RecipesContext';
+import imgHam from "../images/hamburger-phone.png"
 
 function Login() {
   const history = useHistory();
@@ -19,31 +20,42 @@ function Login() {
 
   return (
     <div className="login-page">
-      <h2>Login</h2>
-      <form onSubmit={ onSubmit }>
-        <input
-          name="email"
-          value={ email }
-          onChange={ handleChange }
-          type="email"
-          data-testid="email-input"
-        />
-        <input
-          name="password"
-          value={ password }
-          onChange={ handleChange }
-          type="password"
-          data-testid="password-input"
-        />
-        <button
-          type="submit"
-          disabled={ isSubmitButtonDisabled() }
-          data-testid="login-submit-btn"
-        >
-          Entrar
-        </button>
+      <div></div>
+      <span className="login-login">
+        <h2>Login</h2>
+        <form onSubmit={ onSubmit }>
+          <label htmlFor="email">
+            Email
+            <input
+              id="email"
+              name="email"
+              value={ email }
+              onChange={ handleChange }
+              type="email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input
+              id='password'
+              name="password"
+              value={ password }
+              onChange={ handleChange }
+              type="password"
+              data-testid="password-input"
+            />
+          </label>
+          <button
+            type="submit"
+            disabled={ isSubmitButtonDisabled() }
+            data-testid="login-submit-btn"
+          >
+            Entrar
+          </button>
 
-      </form>
+        </form>
+      </span>
     </div>
 
   );
