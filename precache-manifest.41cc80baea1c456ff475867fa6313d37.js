c4264be48068551ef56f64dd4d4366cde8f1bef2
vitor <tutornews2@gmail.com>
2022-02-01 17:00:14 -0300

    Updates

---
+++ precache-manifest.41cc80baea1c456ff475867fa6313d37.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502e709a5206a790641ace6fe46e205f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da86b044debbf51ca8df",
+    "url": "/static/css/2.34413b16.chunk.css"
+  },
+  {
+    "revision": "dcba74de6eff91a9e6fc",
+    "url": "/static/css/main.fa38ae2f.chunk.css"
+  },
+  {
+    "revision": "da86b044debbf51ca8df",
+    "url": "/static/js/2.35834142.chunk.js"
+  },
+  {
+    "revision": "e16b0378e7a61ce79c4733caf05870f8",
+    "url": "/static/js/2.35834142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcba74de6eff91a9e6fc",
+    "url": "/static/js/main.f5b5c72b.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "28d417808c503eacfe6caea0e878efa5",
+    "url": "/static/media/bussula.28d41780.svg"
+  },
+  {
+    "revision": "bb775e0149a06742c6ff8d7bc534be08",
+    "url": "/static/media/copo.bb775e01.svg"
+  },
+  {
+    "revision": "54de4ec0e858828ed0a7640d665762bb",
+    "url": "/static/media/garfo.54de4ec0.svg"
+  },
+  {
+    "revision": "85b1ed66eae41aa6520ffdc636c025af",
+    "url": "/static/media/hamburger-phone.85b1ed66.png"
+  },
+  {
+    "revision": "e4e6f5fbb46df3bd9961a1861e25b1d3",
+    "url": "/static/media/hamburger.e4e6f5fb.png"
+  },
+  {
+    "revision": "f2a328030879aea57ee4c37cd61c8dec",
+    "url": "/static/media/img01.f2a32803.jpg"
+  },
+  {
+    "revision": "6c78fbb5b86217317b6955f1da809203",
+    "url": "/static/media/img02.6c78fbb5.jpg"
+  },
+  {
+    "revision": "704baa03a2b0bdf0ac0db40751e2de31",
+    "url": "/static/media/profile-photo.704baa03.svg"
+  },
+  {
+    "revision": "434bf82a4b73a64bea73a086be415a83",
+    "url": "/static/media/search.434bf82a.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
